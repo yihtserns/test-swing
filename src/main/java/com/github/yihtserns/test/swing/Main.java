3145f51f99f736351797acf933272102edbe3397
yihtserns <sim_yihtsern@yahoo.com>
2015-06-08 17:02:38 +0800

    Add titled component example.

--- src/main/java/com/github/yihtserns/test/swing/Main.java
+++ src/main/java/com/github/yihtserns/test/swing/Main.java
@@ -1,5 +1,8 @@
 package com.github.yihtserns.test.swing;
 
+import java.awt.Color;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -19,7 +22,11 @@ public class Main {
         JComponent label4 = new JTextField("C4");
         JComponent label5 = new JLabel("C5");
         JComponent label6 = new JTextField("C6");
-        JComponent label7 = new JTextField("C7");
+
+        JComponent label7 = new JPanel();
+        label7.setLayout(new BoxLayout(label7, BoxLayout.LINE_AXIS));
+        label7.add(new JTextField("C7"));
+        label7.setBorder(BorderFactory.createTitledBorder("C7"));
 
         JPanel panel = new JPanel();
         PropertiesLayout layout = new PropertiesLayout(panel);
